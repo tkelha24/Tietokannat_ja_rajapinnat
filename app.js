a98e6a2a5b6fabd35d0426a84d3cf420b923b1f9
tkelha24 <tkelha24@students.oamk.fi>
2025-02-20 13:33:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const db = require('./database');
+const bcrypt = require('bcryptjs');
+const basicAuth = require('express-basic-auth');
 
 const bookRouter = require('./routes/book');
 const borrowerRouter = require('./routes/borrower');
+const userRouter = require('./routes/user');
+const loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -15,6 +20,42 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/book', bookRouter);
+
+app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ))
+
 app.use('/borrower', borrowerRouter);
+app.use('/user', userRouter);
+app.use('/login', loginRouter);
+
+function myAuthorizer(username, password,cb){
+    db.query('SELECT password FROM user WHERE username = ?',[username], 
+      function(dbError, dbResults, fields) {
+        if(dbError){
+              response.json(dbError);
+            }
+        else {
+          if (dbResults.length > 0) {
+            bcrypt.compare(password,dbResults[0].password, 
+              function(err,res) {
+                if(res) {
+                  console.log("success");
+                  return cb(null, true);
+                }
+                else {
+                  console.log("wrong password");
+                  return cb(null, false);
+                }			
+                response.end();
+              }
+            );
+          }
+          else{
+            console.log("user does not exists");
+            return cb(null, false);
+          }
+        }
+      }
+    );
+  }
 
 module.exports = app;
